7830e92badee5c35274fb429b2431c7c6dd86ad0
chriskornaros <chriskornaros@gmail.com>
2024-08-29 17:19:19 -0500

    First python file for the class.

--- CS50p/Lecture0/hello.py
+++ CS50p/Lecture0/hello.py
@@ -1,1 +1,5 @@
-print("Hello, world")
+# Ask the user their name and remove white space from the string and capitalize each word
+name = input("What's your name? ").strip().title()
+
+# Print the user's name
+print(f"Hello, {name}")

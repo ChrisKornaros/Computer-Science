1b330de47c8a88739893ddf9ed44204cb264c794
chriskornaros <chriskornaros@gmail.com>
2024-11-15 17:01:13 -0600

    Cleaning up local.

--- CS50p/Lecture0/hello.py
+++
@@ -1,5 +0,0 @@
-# Ask the user their name and remove white space from the string and capitalize each word
-name = input("What's your name? ").strip().title()
-
-# Print the user's name
-print(f"Hello, {name}")
